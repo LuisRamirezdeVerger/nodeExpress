25affe63cb2a136a6650d9a6285d5e431c2fcba4
Luis RdV <job.wito.uk@gmail.com>
2021-12-13 14:29:21 +0000

    page 2 added

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 
 const app = express();
+const app2 = express();
+const app3 = express();
 
-const port = 5000;
+// Create a variable with the port (as we're using many ports, we don't need to create the variable)
+// const port = 5000;
+// const port1 = 5001;
 
-app.use("/homepage", express.static("public"));
 app.use("/", express.static("public"));
+app.use("/page2", express.static("public/page2.html"));
 
-app.listen(port, () => {
+app.listen(5000, () => {
   console.log("Listening on port :5000");
 });
+
+// app2.use("/homepage", express.static("public"));
+// app2.use("/", express.static("public", { index: "page2.html" }));
+
+// app2.listen(5001, () => {
+//   console.log("Listening on port :5001");
+// });
